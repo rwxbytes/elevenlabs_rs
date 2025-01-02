d35813afbab18d349963a548fd735b44f661be7f
rwxbytes <rwxbytes@proton.me>
2025-01-02 23:48:55 +0000

    refactor: with new endpoint trait def

--- src/endpoints/admin/user.rs
+++ src/endpoints/admin/user.rs
@@ -1,105 +1,135 @@
-#![allow(dead_code)]
 //! The user endpoints
 use super::*;
 
-const USER_PATH: &str = "v1/user";
-const SUBSCRIPTION_PATH: &str = "v1/user/subscription";
-
 /// Gets extended information about the users subscription
+///
 /// # Examples
 /// ```no_run
-///use elevenlabs_rs::*;
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::admin::user::GetUserSubscriptionInfo;
 ///
 /// #[tokio::main]
 /// async fn main() -> Result<()> {
-///     let c = ElevenLabsClient::default()?;
+///     let c = ElevenLabsClient::from_env()?;
 ///     let resp = c.hit(GetUserSubscriptionInfo).await?;
 ///     println!("{:#?}", resp);
 ///     Ok(())
 /// }
 /// ```
+/// See the [Get User Subscription API reference](https://elevenlabs.io/docs/api-reference/user/get-subscription)
 #[derive(Clone, Debug)]
 pub struct GetUserSubscriptionInfo;
 
-impl Endpoint for GetUserSubscriptionInfo {
-    type ResponseBody = Subscription;
+impl ElevenLabsEndpoint for GetUserSubscriptionInfo {
+    const PATH: &'static str = "/v1/user/subscription";
 
     const METHOD: Method = Method::GET;
+
+    type ResponseBody = GetUserSubscriptionInfoResponse;
+
     async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
         Ok(resp.json().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(SUBSCRIPTION_PATH);
-        Ok(url)
     }
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Subscription {
-    tier: String,
-    character_count: i64,
-    character_limit: i64,
-    can_extend_character_limit: bool,
-    allowed_to_extend_character_limit: bool,
-    next_character_count_reset_unix: i64,
-    voice_limit: i64,
-    max_voice_add_edits: i64,
-    voice_add_edit_counter: i64,
-    professional_voice_limit: i64,
-    can_extend_voice_limit: bool,
-    can_use_instant_voice_cloning: bool,
-    can_use_professional_voice_cloning: bool,
-    currency: String,
-    status: String,
-    billing_period: String,
-    character_refresh_period: String,
-    next_invoice: Option<NextInvoice>,
-    has_open_invoices: Option<bool>,
+pub struct GetUserSubscriptionInfoResponse {
+    pub tier: String,
+    pub character_count: i64,
+    pub character_limit: i64,
+    pub can_extend_character_limit: bool,
+    pub allowed_to_extend_character_limit: bool,
+    pub next_character_count_reset_unix: i64,
+    pub voice_limit: i64,
+    pub max_voice_add_edits: Option<i64>,
+    pub voice_add_edit_counter: Option<i64>,
+    pub professional_voice_limit: i64,
+    pub can_extend_voice_limit: bool,
+    pub can_use_instant_voice_cloning: bool,
+    pub can_use_professional_voice_cloning: bool,
+    pub currency: Option<Currency>,
+    pub status: Option<Status>,
+    pub billing_period: Option<BillingPeriod>,
+    pub character_refresh_period: Option<BillingPeriod>,
+    pub next_invoice: Option<NextInvoice>,
+    pub has_open_invoices: Option<bool>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct NextInvoice {
-    amount_due_cents: i64,
-    next_payment_attempt_unix: i64,
+    pub amount_due_cents: i64,
+    pub next_payment_attempt_unix: i64,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Currency {
+    Usd,
+    Eur,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Status {
+    Trailing,
+    Active,
+    Incomplete,
+    IncompleteExpired,
+    Cancelled,
+    PastDue,
+    Unpaid,
+    Free,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum BillingPeriod {
+    MonthlyPeriod,
+    AnnualPeriod,
 }
 
 /// Gets information about the user
+///
 /// # Examples
 /// ```no_run
-/// use elevenlabs_rs::*;
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::admin::user::GetUserInfo;
 ///
 /// #[tokio::main]
 /// async fn main() -> Result<()> {
-///    let c = ElevenLabsClient::default()?;
+///    let c = ElevenLabsClient::from_env()?;
 ///    let resp = c.hit(GetUserInfo).await?;
 ///    println!("{:#?}", resp);
 ///   Ok(())
 /// }
 /// ```
+/// See the [Get User API reference](https://elevenlabs.io/docs/api-reference/user/get)
 #[derive(Clone, Debug)]
 pub struct GetUserInfo;
 
-impl Endpoint for GetUserInfo {
-    type ResponseBody = UserInfo;
+impl ElevenLabsEndpoint for GetUserInfo {
+    const PATH: &'static str = "/v1/user";
 
     const METHOD: Method = Method::GET;
+
+    type ResponseBody = GetUserInfoResponse;
+
     async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
         Ok(resp.json().await?)
     }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(USER_PATH);
-        Ok(url)
-    }
 }
 #[derive(Clone, Debug, Deserialize)]
-pub struct UserInfo {
-    subscription: Subscription,
-    is_new_user: bool,
-    xi_api_key: String,
-    can_use_delayed_payment_methods: bool,
-    is_onboarding_completed: bool,
-    is_onboarding_checklist_completed: bool,
-    first_name: Option<String>,
+pub struct GetUserInfoResponse {
+    pub subscription: GetUserSubscriptionInfoResponse,
+    pub subscription_extras: Value,
+    pub is_new_user: bool,
+    pub xi_api_key: String,
+    pub can_use_delayed_payment_methods: bool,
+    pub is_onboarding_completed: bool,
+    pub is_onboarding_checklist_completed: bool,
+    pub first_name: Option<String>,
+    pub is_api_key_hashed: Option<bool>,
+    pub xi_api_key_preview: Option<String>,
+    pub referral_link_code: Option<String>,
+    pub partnerstack_partner_default_link: Option<String>,
 }
