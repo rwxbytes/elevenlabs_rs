32a95a50181b28e331bcf026553466b76d696906
rwxbytes <rwxbytes@proton.me>
2025-01-02 01:54:40 +0000

    refactor<history.rs> with new endpoint trait def

--- src/endpoints/admin/history.rs
+++ src/endpoints/admin/history.rs
@@ -1,293 +1,86 @@
 //! The history endpoints
-#![allow(dead_code)]
 
 use super::*;
-use crate::endpoints::voice::VoiceSettings;
+use crate::components::admin::history::*;
+use crate::components::common::{VoiceCategory, VoiceSettings};
 
-const HISTORY_PATH: &str = "/v1/history";
-const AUDIO_PATH: &str = "/audio";
-const PAGE_SIZE_QUERY: &str = "page_size";
-const HISTORY_ITEM_IDS: &str = "history_item_ids";
-const START_AFTER_HISTORY_ITEM_ID_QUERY: &str = "start_after_history_item_id";
-const VOICE_ID_QUERY: &str = "voice_id";
-
-#[derive(Clone, Debug)]
-pub struct DeleteHistoryItem(HistoryItemID);
-
-impl DeleteHistoryItem {
-    pub fn new<T: Into<String>>(history_item_id: T) -> Self {
-        Self(HistoryItemID(history_item_id.into()))
-    }
-}
-
-impl Endpoint for DeleteHistoryItem {
-    type ResponseBody = StatusResponseBody;
-    const METHOD: Method = Method::DELETE;
-    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
-        Ok(resp.json().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(&format!("{}/{}", HISTORY_PATH, self.0 .0));
-        Ok(url)
-    }
-}
-
-/// Download one or more history items.
-/// If one history item ID is provided, we will return a single audio file.
-/// If more than one history item IDs are provided, we will provide the history items packed into a .zip file.
+/// Returns metadata about all your generated audio.
 ///
 /// # Example
 /// ```no_run
-/// use elevenlabs_rs::*;
-/// use elevenlabs_rs::utils::save;
+/// use elevenlabs_rs::{ElevenLabsClient, Result, DefaultVoice};
+/// use elevenlabs_rs::endpoints::admin::history::{GetGeneratedItems, HistoryQuery};
 ///
 /// #[tokio::main]
 /// async fn main() -> Result<()> {
-///     let client = ElevenLabsClient::default()?;
-///     let history_item_ids = client
-///         .hit(GetGeneratedItems::new(HistoryQuery::default()))
-///         .await?
-///         .history()
-///         .iter()
-///         .map(|i| i.history_item_id().to_string())
-///         .collect::<Vec<String>>();
-///     let body = DownloadBody::new(history_item_ids);
-///     let downloaded_items = client.hit(DownloadHistoryItems::new(body)).await?;
-///     save("last_100_items.zip", downloaded_items)?;
-///     Ok(())
-/// }
-/// ```
-#[derive(Clone, Debug)]
-pub struct DownloadHistoryItems(DownloadBody);
-
-impl DownloadHistoryItems {
-    pub fn new(body: DownloadBody) -> Self {
-        Self(body)
-    }
-}
-
-impl Endpoint for DownloadHistoryItems {
-    type ResponseBody = Bytes;
-    const METHOD: Method = Method::POST;
-    async fn request_body(&self) -> Result<RequestBody> {
-        Ok(RequestBody::Json(serde_json::to_value(&self.0)?))
-    }
-
-    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
-        Ok(resp.bytes().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(&format!("{}{}", HISTORY_PATH, DOWNLOAD_PATH));
-        Ok(url)
-    }
-}
-#[derive(Clone, Debug, Serialize)]
-pub struct DownloadBody {
-    pub history_item_ids: Vec<String>,
-    /// Output format to transcode the audio file, can be wav or default (Mp3).
-    #[serde(skip_serializing_if = "DownloadOutputFormat::is_mp3")]
-    pub output_format: DownloadOutputFormat,
-}
-
-impl DownloadBody {
-    pub fn new(history_item_ids: Vec<String>) -> Self {
-        Self  {
-            history_item_ids,
-            output_format: DownloadOutputFormat::default(),
-        }
-    }
-    pub fn with_output_format(mut self, output_format: DownloadOutputFormat) -> Self {
-        self.output_format = output_format;
-        self
-    }
-}
-
-#[derive(Clone, Debug, Default, Serialize)]
-#[serde(rename_all = "lowercase")]
-pub enum DownloadOutputFormat {
-    #[default]
-    Mp3,
-    Wav,
-}
-
-impl DownloadOutputFormat {
-    fn is_mp3(&self) -> bool {
-        match self {
-            DownloadOutputFormat::Mp3 => true,
-            DownloadOutputFormat::Wav => false,
-        }
-    }
-}
-
-/// Get the audio file of a history item.
-///
-/// # Example
-/// ```no_run
-/// use elevenlabs_rs::*;
-/// use elevenlabs_rs::utils::play;
-///
-/// #[tokio::main]
-/// async fn main() -> Result<()> {
-///     let client = ElevenLabsClient::default()?;
-///     let query = HistoryQuery::default().with_page_size(1);
-///     let resp = client.hit(GetGeneratedItems::new(query))
-///         .await?;
-///     let item_id = resp
-///         .history()
-///         .first().unwrap()
-///         .history_item_id();
-///     let audio = client.hit(GetAudio::new(item_id)).await?;
-///     play(audio)?;
-///     Ok(())
-/// }
-/// ```
-#[derive(Clone, Debug)]
-pub struct GetAudio(HistoryItemID);
-
-impl GetAudio {
-    pub fn new<T: Into<String>>(history_item_id: T) -> Self {
-        Self(HistoryItemID(history_item_id.into()))
-    }
-}
-
-impl Endpoint for GetAudio {
-    type ResponseBody = Bytes;
-    const METHOD: Method = Method::GET;
-    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
-        Ok(resp.bytes().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(&format!("{}/{}{}", HISTORY_PATH, self.0 .0, AUDIO_PATH));
-        Ok(url)
-    }
-}
-
-/// Get the generated items endpoint.
-///
-/// # Example
-/// ```no_run
-/// use elevenlabs_rs::*;
-///
-/// #[tokio::main]
-/// async fn main() -> Result<()> {
-///     let c = ElevenLabsClient::default()?;
-///     let query = HistoryQuery::default()
+///    let c = ElevenLabsClient::default()?;
+///    let query = HistoryQuery::default()
 ///         .with_page_size(10)
-///         .with_voice_id(LegacyVoice::Alice);
-///     let endpoint = GetGeneratedItems::new(query);
-///     let resp = c.hit(endpoint).await?;
-///     println!("{:#?}", resp);
-///     Ok(())
+///         .with_voice_id(DefaultVoice::Brian);
+///    let endpoint = GetGeneratedItems::with_query(query);
+///    let resp = c.hit(endpoint).await?;
+///    println!("{:#?}", resp);
+///    Ok(())
 /// }
 /// ```
-#[derive(Clone, Debug)]
-pub struct GetGeneratedItems(HistoryQuery);
+/// See the [Get Generated Items API reference ](https://elevenlabs.io/docs/api-reference/history/get-all)
+#[derive(Clone, Debug, Default)]
+pub struct GetGeneratedItems {
+    query: Option<HistoryQuery>,
+}
 
 impl GetGeneratedItems {
-    pub fn new(query: HistoryQuery) -> Self {
-        Self(query)
-    }
-}
-
-impl Endpoint for GetGeneratedItems {
-    type ResponseBody = GeneratedItems;
-    const METHOD: Method = Method::GET;
-    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
-        Ok(resp.json().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut ego = self.clone();
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(HISTORY_PATH);
-        url.set_query(ego.0.join().as_deref());
-        Ok(url)
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct GetHistoryItem(HistoryItemID);
-
-impl GetHistoryItem {
-    pub fn new<T: Into<String>>(history_item_id: T) -> Self {
-        Self(HistoryItemID(history_item_id.into()))
-    }
-}
-
-impl Endpoint for GetHistoryItem {
-    type ResponseBody = HistoryItem;
-    const METHOD: Method = Method::GET;
-    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
-        Ok(resp.json().await?)
-    }
-    fn url(&self) -> Result<Url> {
-        let mut url = BASE_URL.parse::<Url>().unwrap();
-        url.set_path(&format!("{}/{}", HISTORY_PATH, self.0 .0));
-        Ok(url)
+    pub fn with_query(query: HistoryQuery) -> Self {
+        Self { query: Some(query) }
     }
 }
 
 #[derive(Clone, Debug, Default)]
 pub struct HistoryQuery {
-    pub page_size: Option<String>,
-    pub start_after_history_item_id: Option<String>,
-    pub voice_id: Option<String>,
+    params: QueryValues,
 }
 
 impl HistoryQuery {
     pub fn with_page_size(mut self, page_size: u16) -> Self {
-        self.page_size = Some(format!("{}={}", PAGE_SIZE_QUERY, page_size));
+        self.params.push(("page_size", page_size.to_string()));
         self
     }
     pub fn with_start_after_history_item_id(mut self, start_after_history_item_id: &str) -> Self {
-        self.start_after_history_item_id = Some(format!(
-            "{}={}",
-            START_AFTER_HISTORY_ITEM_ID_QUERY, start_after_history_item_id
+        self.params.push((
+            "start_after_history_item_id",
+            start_after_history_item_id.to_string(),
         ));
         self
     }
 
-    pub fn with_voice_id<T: Into<String>>(mut self, voice_id: T) -> Self {
-        self.voice_id = Some(format!("{}={}", VOICE_ID_QUERY, voice_id.into()));
+    pub fn with_voice_id(mut self, voice_id: impl Into<VoiceID>) -> Self {
+        self.params.push(("voice_id", voice_id.into()._inner));
         self
     }
-    pub fn join(&mut self) -> Option<String> {
-        let mut result = String::new();
+}
 
-        if let Some(value) = self.page_size.take() {
-            result.push_str(&value);
-        }
-        if let Some(value) = self.start_after_history_item_id.take() {
-            if !result.is_empty() {
-                result.push('&');
-            }
-            result.push_str(&value);
-        }
-        if let Some(value) = self.voice_id.take() {
-            if !result.is_empty() {
-                result.push('&');
-            }
-            result.push_str(&value);
-        }
-        if result.is_empty() {
-            None
-        } else {
-            Some(result)
-        }
+impl ElevenLabsEndpoint for GetGeneratedItems {
+    const PATH: &'static str = "/v1/history";
+    const METHOD: Method = Method::GET;
+    type ResponseBody = GetGeneratedItemsResponse;
+
+    fn query_params(&self) -> Option<QueryValues> {
+        self.query.as_ref().map(|q| q.params.clone())
+    }
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.json().await?)
     }
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct GeneratedItems {
-    history: Vec<HistoryItem>,
+pub struct GetGeneratedItemsResponse {
+    history: Vec<GetHistoryItemResponse>,
     last_history_item_id: String,
     has_more: bool,
 }
 
-impl GeneratedItems {
-    pub fn history(&self) -> &Vec<HistoryItem> {
+impl GetGeneratedItemsResponse {
+    pub fn history(&self) -> &[GetHistoryItemResponse] {
         &self.history
     }
     pub fn last_history_item_id(&self) -> &str {
@@ -298,13 +91,56 @@ impl GeneratedItems {
     }
 }
 
+/// Returns information about a history item by its ID.
+///
+/// # Example
+/// ```no_run
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::admin::history::GetHistoryItem;
+///
+/// #[tokio::main]
+/// async fn main() -> Result<()> {
+///    let client = ElevenLabsClient::default()?;
+///    let history_item_id = "some_history_item_id";
+///    let resp = client.hit(GetHistoryItem::new(history_item_id)).await?;
+///    println!("{:#?}", resp);
+///    Ok(())
+/// }
+/// ```
+/// See the [Get History Item API reference](https://elevenlabs.io/docs/api-reference/history/get)
+#[derive(Clone, Debug)]
+pub struct GetHistoryItem {
+    history_item_id: HistoryItemID,
+}
+
+impl GetHistoryItem {
+    pub fn new(history_item_id: impl Into<HistoryItemID>) -> Self {
+        Self {
+            history_item_id: history_item_id.into(),
+        }
+    }
+}
+
+impl ElevenLabsEndpoint for GetHistoryItem {
+    const PATH: &'static str = "/v1/history/:history_item_id";
+    const METHOD: Method = Method::GET;
+    type ResponseBody = GetHistoryItemResponse;
+
+    fn path_params(&self) -> Vec<(&'static str, &str)> {
+        vec![self.history_item_id.as_path_param()]
+    }
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.json().await?)
+    }
+}
+
 #[derive(Clone, Debug, Deserialize)]
-pub struct HistoryItem {
+pub struct GetHistoryItemResponse {
     history_item_id: String,
     request_id: String,
     voice_id: String,
     voice_name: String,
-    voice_category: Option<String>,
+    voice_category: Option<VoiceCategory>,
     model_id: Option<String>,
     text: String,
     date_unix: u64,
@@ -315,9 +151,11 @@ pub struct HistoryItem {
     settings: VoiceSettings,
     feedback: Option<Feedback>,
     share_link_id: Option<String>,
-    source: Option<String>,
+    source: Option<Source>,
+    // TODO: impl type
+    alignments: Option<Value>,
 }
-impl HistoryItem {
+impl GetHistoryItemResponse {
     pub fn history_item_id(&self) -> &str {
         &self.history_item_id
     }
@@ -330,8 +168,8 @@ impl HistoryItem {
     pub fn voice_name(&self) -> &str {
         &self.voice_name
     }
-    pub fn voice_category(&self) -> Option<&str> {
-        self.voice_category.as_deref()
+    pub fn voice_category(&self) -> Option<&VoiceCategory> {
+        self.voice_category.as_ref()
     }
     pub fn model_id(&self) -> Option<&str> {
         self.model_id.as_deref()
@@ -363,45 +201,183 @@ impl HistoryItem {
     pub fn share_link_id(&self) -> Option<&str> {
         self.share_link_id.as_deref()
     }
-    pub fn source(&self) -> Option<&str> {
-        self.source.as_deref()
+    pub fn source(&self) -> Option<&Source> {
+        self.source.as_ref()
+    }
+    pub fn alignments(&self) -> Option<&Value> {
+        self.alignments.as_ref()
     }
 }
 
-#[derive(Clone, Debug, Deserialize)]
-pub struct Feedback {
-    thumbs_up: bool,
-    feedback: String,
-    emotions: bool,
-    inaccurate_clone: bool,
-    glitches: bool,
-    audio_quality: bool,
-    review_status: String,
-    other: bool,
+/// Delete a history item by its ID.
+///
+/// See the [Delete History Item API reference](https://elevenlabs.io/docs/api-reference/history/delete)
+#[derive(Clone, Debug)]
+pub struct DeleteHistoryItem {
+    history_item_id: HistoryItemID,
 }
-impl Feedback {
-    pub fn thumbs_up(&self) -> bool {
-        self.thumbs_up
+
+impl DeleteHistoryItem {
+    pub fn new(history_item_id: impl Into<HistoryItemID>) -> Self {
+        Self {
+            history_item_id: history_item_id.into(),
+        }
     }
-    pub fn feedback(&self) -> &str {
-        &self.feedback
+}
+
+impl ElevenLabsEndpoint for DeleteHistoryItem {
+    const PATH: &'static str = "/v1/history/:history_item_id";
+
+    const METHOD: Method = Method::DELETE;
+
+    type ResponseBody = StatusResponseBody;
+
+    fn path_params(&self) -> Vec<(&'static str, &str)> {
+        vec![self.history_item_id.as_path_param()]
     }
-    pub fn emotions(&self) -> bool {
-        self.emotions
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.json().await?)
     }
-    pub fn inaccurate_clone(&self) -> bool {
-        self.inaccurate_clone
+}
+
+/// Download one or more history items.
+/// If one history item ID is provided, we will return a single audio file.
+/// If more than one history item IDs are provided, we will provide the history items packed into a .zip file.
+///
+/// # Example
+/// ```no_run
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::admin::history::{
+///     DownloadBody, DownloadHistoryItems, HistoryQuery, GetGeneratedItems};
+/// use elevenlabs_rs::utils::save;
+///
+/// #[tokio::main]
+/// async fn main() -> Result<()> {
+///     let client = ElevenLabsClient::default()?;
+///     let history_item_ids = client
+///         .hit(GetGeneratedItems::with_query(HistoryQuery::default()))
+///         .await?
+///         .history()
+///         .iter()
+///         .map(|i| i.history_item_id().to_string())
+///         .collect::<Vec<String>>();
+///     let body = DownloadBody::new(history_item_ids);
+///     let downloaded_items = client.hit(DownloadHistoryItems::new(body)).await?;
+///     save("last_100_items.zip", downloaded_items)?;
+///     Ok(())
+/// }
+/// ```
+/// See the [Download History Items API reference](https://elevenlabs.io/docs/api-reference/history/download)
+#[derive(Clone, Debug)]
+pub struct DownloadHistoryItems {
+    body: DownloadBody,
+}
+
+impl DownloadHistoryItems {
+    pub fn new(body: DownloadBody) -> Self {
+        Self { body }
     }
-    pub fn glitches(&self) -> bool {
-        self.glitches
+}
+
+impl ElevenLabsEndpoint for DownloadHistoryItems {
+    const PATH: &'static str = "/v1/history/download";
+
+    const METHOD: Method = Method::POST;
+
+    type ResponseBody = Bytes;
+
+    async fn request_body(&self) -> Result<RequestBody> {
+        Ok(RequestBody::Json(serde_json::to_value(&self.body)?))
     }
-    pub fn audio_quality(&self) -> bool {
-        self.audio_quality
+
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.bytes().await?)
     }
-    pub fn review_status(&self) -> &str {
-        &self.review_status
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct DownloadBody {
+    pub history_item_ids: Vec<String>,
+    /// Output format to transcode the audio file, can be wav or default (Mp3).
+    #[serde(skip_serializing_if = "DownloadOutputFormat::is_mp3")]
+    pub output_format: DownloadOutputFormat,
+}
+
+impl DownloadBody {
+    pub fn new(history_item_ids: Vec<String>) -> Self {
+        Self {
+            history_item_ids,
+            output_format: DownloadOutputFormat::default(),
+        }
     }
-    pub fn other(&self) -> bool {
-        self.other
+    pub fn with_output_format(mut self, output_format: DownloadOutputFormat) -> Self {
+        self.output_format = output_format;
+        self
+    }
+}
+
+/// Returns the audio of a history item.
+///
+/// # Example
+/// ```no_run
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::admin::history::{GetGeneratedItems, GetAudio, HistoryQuery};
+/// use elevenlabs_rs::utils::play;
+///
+/// #[tokio::main]
+/// async fn main() -> Result<()> {
+///     let client = ElevenLabsClient::default()?;
+///     let query = HistoryQuery::default().with_page_size(1);
+///     let resp = client.hit(GetGeneratedItems::with_query(query)).await?;
+///     let item_id = resp.into_iter().next().unwrap().history_item_id();
+///     let audio = client.hit(GetAudio::new(item_id)).await?;
+///     play(audio)?;
+///     Ok(())
+/// }
+/// ```
+#[derive(Clone, Debug)]
+pub struct GetAudio {
+    history_item_id: HistoryItemID,
+}
+
+impl GetAudio {
+    pub fn new(history_item_id: impl Into<HistoryItemID>) -> Self {
+        Self {
+            history_item_id: history_item_id.into(),
+        }
+    }
+}
+
+impl ElevenLabsEndpoint for GetAudio {
+    const PATH: &'static str = "/v1/history/:history_item_id/audio";
+
+    const METHOD: Method = Method::GET;
+
+    type ResponseBody = Bytes;
+
+    fn path_params(&self) -> Vec<(&'static str, &str)> {
+        vec![self.history_item_id.as_path_param()]
+    }
+
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.bytes().await?)
+    }
+}
+
+impl<'a> IntoIterator for &'a GetGeneratedItemsResponse {
+    type Item = &'a GetHistoryItemResponse;
+    type IntoIter = std::slice::Iter<'a, GetHistoryItemResponse>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.history.iter()
+    }
+}
+
+impl IntoIterator for GetGeneratedItemsResponse {
+    type Item = GetHistoryItemResponse;
+    type IntoIter = std::vec::IntoIter<GetHistoryItemResponse>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.history.into_iter()
     }
 }
