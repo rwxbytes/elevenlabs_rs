0394e3c8d45be623bf7e447d9cdb0e88f3650c42
rwxbytes <rwxbytes@proton.me>
2025-02-06 17:12:12 +0000

    feat: added GetTool endpoint

--- elevenlabs_rs/src/endpoints/convai/tools.rs
+++ elevenlabs_rs/src/endpoints/convai/tools.rs
@@ -19,6 +19,7 @@ use crate::endpoints::convai::knowledge_base::DependentAgent;
 ///     Ok(())
 /// }
 /// ```
+/// See [List Tools API reference](https://elevenlabs.io/docs/api-reference/tools/get-tools).
 pub struct ListTools;
 
 impl ElevenLabsEndpoint for ListTools {
@@ -37,6 +38,50 @@ impl ElevenLabsEndpoint for ListTools {
 #[derive(Clone, Debug, Deserialize)]
 pub struct ListToolsResponse {
     pub tools: Vec<GetToolResponse>,
+}
+
+/// Get tool that is available in the workspace.
+///
+/// # Example
+/// ```no_run
+/// use elevenlabs_rs::{ElevenLabsClient, Result};
+/// use elevenlabs_rs::endpoints::convai::tools::GetTool;
+///
+/// #[tokio::main]
+/// async fn main() -> Result<()> {
+///    let c = ElevenLabsClient::from_env()?;
+///    let resp = c.hit(GetTool::new("tool_id")).await?;
+///    println!("{:#?}", resp);
+///    Ok(())
+/// }
+/// ```
+/// # See [Get Tool API reference](https://elevenlabs.io/docs/api-reference/tools/get-tool).
+pub struct GetTool {
+    tool_id: String,
+}
+
+impl GetTool {
+    pub fn new(tool_id: impl Into<String>) -> Self {
+        Self {
+            tool_id: tool_id.into(),
+        }
+    }
+}
+
+impl ElevenLabsEndpoint for GetTool {
+    const PATH: &'static str = "/v1/convai/tools/:tool_id";
+
+    const METHOD: Method = Method::GET;
+
+    type ResponseBody = GetToolResponse;
+
+    fn path_params(&self) -> Vec<(&'static str, &str)> {
+        vec![self.tool_id.and_param(PathParam::ToolID)]
+    }
+
+    async fn response_body(self, resp: Response) -> Result<Self::ResponseBody> {
+        Ok(resp.json().await?)
+    }
 }
 
 #[derive(Clone, Debug, Deserialize)]
